78bef332f3c22c5f3609598d94aab4450b637517
amFastBoi <conyersbusiness@gmail.com>
2020-04-21 12:45:56 +0100

    RGB color set is the class used to set the rgb colors

---
+++ lib/rgbColorSet/rgbColorSet.hpp
@@ -0,0 +1,9 @@
+void tempratureSetupClass();
+int tempReturn();
+void RGBTempClass();
+void humiditySetUpClass();
+int humReturn();
+void RGBHumClass();
+void humiditySetUpClass();
+int humReturn();
+void RGBHumClass();
